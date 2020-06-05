b68b831faf5912fb0dfc3aed38f09d7dfa806ba2
Stevie-P72 <stevier72@outlook.com>
2020-06-05 21:19:28 +0000

    sw

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -9,10 +9,8 @@ connection = pymysql.connect(host='localhost',
                              password='',
                              db='Chinook')
 try:
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        cursor.execute("Update Friends set age = 22 where name = 'Bob';")
+        connection.commit()
 finally:
     connection.close()
